752353e87b1abee7ec3ecb9c3b8063c8d457e3de
SHIMOKAWAAYAKA <shimokawaayaka@Shinsuke-no-MacBook-Pro.local>
2016-05-23 12:59:25 +0900

    add judge page

--- app/controllers/practice_controller.rb
+++ app/controllers/practice_controller.rb
@@ -2,4 +2,10 @@ class PracticeController < ApplicationController
   def index
 
   end
+
+
+  def judge
+    @choice_id = params[:choice]
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,55 @@
 q = Question.new(sentence: "ayaka (  ) happy." , explanation: "cuz...")
 q.save
 
-ans = Choice.new(question_id: q.id , sub_sentence: "is" , answer: "o")
-ans.save
+Choice.new(question_id: q.id , sub_sentence: "is" , answer: "o").save
 Choice.new(question_id: q.id , sub_sentence: "are" , answer: "x").save
 Choice.new(question_id: q.id , sub_sentence: "am" , answer: "x").save
 Choice.new(question_id: q.id , sub_sentence: "be" , answer: "x").save
 
+q = Question.new(sentence: "Operators are able to handle customer inquiries ------ the telephone or online." ,
+                 explanation: "正解はover。空所の後ろはthe telephoneという名詞句なので、前には意味の合う前置詞を選ぶ。over the telephone「電話を通じて」という表現になる。")
+q.save
+
+Choice.new(question_id: q.id , sub_sentence: "over" , answer: "o").save
+Choice.new(question_id: q.id , sub_sentence: "either" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "by" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "whether" , answer: "x").save
+
+q = Question.new(sentence: "Now that I ------- the opportunity to meet you and review your qualifications in detail, I'm afraid that we need someone with more experience." ,
+                 explanation: "正解はhave had。Now that I ------- the opportunity to meet you「お会いするチャンスに恵まれ」という表現の定型文。")
+q.save
+
+Choice.new(question_id: q.id , sub_sentence: "have had" , answer: "o").save
+Choice.new(question_id: q.id , sub_sentence: "will be getting" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "have" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "will have had" , answer: "x").save
+
+q = Question.new(sentence: "Many critics consider the film to be one of the ------ romantic film ever made." ,
+                 explanation: "正解はbest。本文中のever made「今まで作られた中で(1番)」という単語があるので、最上級であるbestを選ぶ。well、goodは原級、betterは比較級である。
+")
+q.save
+
+Choice.new(question_id: q.id , sub_sentence: "best" , answer: "o").save
+Choice.new(question_id: q.id , sub_sentence: "well" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "good" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "better" , answer: "x").save
+
+q = Question.new(sentence: "Mr. Minegishi was extremely nervous before giving ------ at the sales conference last November." ,
+                 explanation: "正解はpresentations。空欄には直前にある動名詞のgivingの目的語として名詞が必要である。空欄の前に冠詞がないので、複数形を選ぶ。give presentations「プレゼンを行う」はよく使う。")
+q.save
+
+Choice.new(question_id: q.id , sub_sentence: "have had" , answer: "o").save
+Choice.new(question_id: q.id , sub_sentence: "will be getting" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "have" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "will have had" , answer: "x").save
+
+q = Question.new(sentence: "In ------ one year, our company became the second largest company in the swimsuit industry.
+" ,
+                 explanation: "答えはjust。直後にあるoneは数詞であり、数詞を修飾する副詞として、ふさわしいのはjustである。just one year「たった1年」は頻出表現である。
+")
+q.save
+
+Choice.new(question_id: q.id , sub_sentence: "just" , answer: "o").save
+Choice.new(question_id: q.id , sub_sentence: "recently" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "alone" , answer: "x").save
+Choice.new(question_id: q.id , sub_sentence: "thoroughly" , answer: "x").save
